a38c863b17eb6b440bf92ddc2d484e3d3f8ab6ce
Arthur Granado <agranado@pluga.co>
2017-07-27 16:18:25 -0300

    Create IP list for each page. The IP list will include repeated IPs

--- test/parse_test.rb
+++ test/parse_test.rb
@@ -5,6 +5,11 @@ class Parser
     parts = line.split(" ")
     {page: parts[0], ip: parts[1]}
   end
+
+  def include_ip_page_list(page_ip_list, entry)
+    page_ip_list[entry[:page]] = page_ip_list.fetch(entry[:page],[]) << entry[:ip]
+    page_ip_list
+  end
 end
 
 class ParserTest < Minitest::Test
@@ -24,4 +29,26 @@ class ParserTest < Minitest::Test
     assert_equal result, {page: "/about/2", ip: "444.701.448.104"}
   end
 
+  def test_include_ip_in_page_list_for_one_entry
+    entry = {page: "/home", ip: "184.123.665.067"}
+    page_ip_list = {}
+
+    page_ip_list = Parser.new.include_ip_page_list(page_ip_list, entry)
+
+    assert_equal page_ip_list, {"/home" => ["184.123.665.067"]}
+  end
+
+  def test_include_ip_in_page_list_from_three_log_entries
+    page_views = {}
+    entries = [ {page: "/home", ip: "184.123.665.067"},
+                {page: "/about/2", ip: "444.701.44.104"},
+                {page: "/home", ip: "184.123.665.067"}]
+
+    entries.each do |entry|
+      page_views = Parser.new.include_ip_page_list(page_views, entry)
+    end
+
+    assert_equal page_views, {"/home" => ["184.123.665.067","184.123.665.067"],
+                              "/about/2" => ["444.701.44.104"]}
+  end
 end
