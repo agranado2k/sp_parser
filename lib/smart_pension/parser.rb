f3b756b58f7f4a4e74c115299039e012fdf29e66
Arthur Granado <agranado@pluga.co>
2017-07-27 21:40:49 -0300

    Parse - create method to create data from log entries

--- lib/smart_pension/parser.rb
+++ lib/smart_pension/parser.rb
@@ -1,13 +1,17 @@
 module SmartPension
   class Parser
+    def from_entries(entries)
+      entries.reduce({}){|r, entry| r = from_entry(r, entry)}
+    end
+
+    def from_entry(page_ip_list, entry)
+      page_ip_list[entry[:page]] = page_ip_list.fetch(entry[:page],[]) << entry[:ip]
+      page_ip_list
+    end
+
     def split_page_and_ip(line)
       parts = line.split(" ")
       {page: parts[0], ip: parts[1]}
-    end
-
-    def include_ip_page_list(page_ip_list, entry)
-      page_ip_list[entry[:page]] = page_ip_list.fetch(entry[:page],[]) << entry[:ip]
-      page_ip_list
     end
   end
 end
--- test/smart_pension/parser_test.rb
+++ test/smart_pension/parser_test.rb
@@ -21,24 +21,36 @@ class ParserTest < Minitest::Test
     assert_equal result, {page: "/about/2", ip: "444.701.448.104"}
   end
 
-  def test_include_ip_in_page_list_for_one_entry
+  def test_parse_from_one_entry
     entry = {page: "/home", ip: "184.123.665.067"}
     page_ip_list = {}
 
-    page_ip_list = @parser.include_ip_page_list(page_ip_list, entry)
+    page_ip_list = @parser.from_entry(page_ip_list, entry)
 
     assert_equal page_ip_list, {"/home" => ["184.123.665.067"]}
   end
 
-  def test_include_ip_in_page_list_from_three_log_entries
+  def test_parse_from_some_entries
     page_views = {}
     entries = [ {page: "/home", ip: "184.123.665.067"},
                 {page: "/about/2", ip: "444.701.44.104"},
                 {page: "/home", ip: "184.123.665.067"}]
 
     entries.each do |entry|
-      page_views = @parser.include_ip_page_list(page_views, entry)
+      page_views = @parser.from_entry(page_views, entry)
     end
+
+    assert_equal page_views, {"/home" => ["184.123.665.067","184.123.665.067"],
+                              "/about/2" => ["444.701.44.104"]}
+  end
+
+  def test_parse_entries
+    page_views = {}
+    entries = [ {page: "/home", ip: "184.123.665.067"},
+                {page: "/about/2", ip: "444.701.44.104"},
+                {page: "/home", ip: "184.123.665.067"}]
+
+    page_views = @parser.from_entries(entries)
 
     assert_equal page_views, {"/home" => ["184.123.665.067","184.123.665.067"],
                               "/about/2" => ["444.701.44.104"]}
